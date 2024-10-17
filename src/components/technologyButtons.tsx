20b0f55e2c5a0af8488399f625fd2b773089955e
Fernando Gazzana <fernando.gazzana@gmail.com>
2024-10-17 20:13:04 -0300

    Modal implementado

--- src/components/technologyButtons.tsx
+++ src/components/technologyButtons.tsx
@@ -1,7 +1,29 @@
-import {ReactElement} from "react";
+import {ReactElement, useState} from "react";
 import "../App.css";
+import texts from "../assets/texts/texts.json"
+import {
+    Modal,
+    ModalBody,
+    ModalCloseButton,
+    ModalContent,
+    ModalHeader,
+    ModalOverlay,
+    useDisclosure
+} from "@chakra-ui/react";
 
 export function TechnologyButtons() {
+
+    class Technologies {
+        technology: string;
+        url: string;
+        description: string;
+
+        constructor(technology: string, url: string, description: string) {
+            this.technology = technology;
+            this.url = url;
+            this.description = description;
+        }
+    }
 
     const buttonContainerProps = {
         width: '450px',
@@ -23,27 +45,101 @@ export function TechnologyButtons() {
         boxShadow: ' 1px 1px 1px #293A80',
     }
 
-    const buttons: string[] = [
+    const js = new Technologies(
+        'JavaScript',
         'src/assets/javascript-icon.svg',
+        texts.js
+    )
+    const css = new Technologies(
+        'CSS',
         'src/assets/css-icon.svg',
+        texts.css
+    )
+    const html = new Technologies(
+        'HTML',
         'src/assets/html-icon.svg',
+        texts.html
+    )
+    const react = new Technologies(
+        'ReactJS',
         'src/assets/react-icon.svg',
+        texts.react
+    )
+    const java = new Technologies(
+        'Java',
         'src/assets/java-icon.svg',
+        texts.java
+    )
+    const kotlin = new Technologies(
+        'Kotlin',
         'src/assets/kotlin-icon.svg',
+        texts.kotlin
+    )
+    const intellij = new Technologies(
+        'IntelliJ IDEA',
         'src/assets/intellij-icon.svg',
-        'src/assets/github-142-svgrepo-com.svg',
+        texts.intellij
+    )
+    const git = new Technologies(
+        'Git',
+        'src/assets/git-icon.svg',
+        texts.git
+    )
+
+    const buttons: Technologies[] = [
+        js,
+        css,
+        html,
+        react,
+        java,
+        kotlin,
+        intellij,
+        git,
     ]
 
-    const genButton = (buttons: string[]) => {
+
+    const {isOpen, onOpen, onClose} = useDisclosure();
+    const [activeIndex, setActiveIndex] = useState<number | null>(null);
+
+    const genButton = (buttons: Technologies[]) => {
         const b: ReactElement[] = [];
-        buttons.forEach((button) => {
+
+        buttons.forEach((button, index) => {
             b.push(
-                <button style={buttonProps}
-                    className={"button"}
-                >
-                    <a href={""}/>
-                    <img width={48} height={48} src={button}/>
-                </button>
+                <div key={index}>
+
+                    <button style={buttonProps}
+                            onClick={() => {
+                                setActiveIndex(index);
+                                onOpen();
+                            }}
+                            className={"button"}
+                    >
+                        <img width={48} height={48} src={button.url} alt="Technology Icon"/>
+                    </button>
+
+                    <Modal
+                        closeOnOverlayClick={true}
+                        isOpen={isOpen && activeIndex === index}
+                        onClose={() => {
+                            setActiveIndex(null);
+                            onClose();
+                        }}
+                        isCentered
+                    >
+                        <ModalOverlay/>
+                        <ModalContent>
+                            <ModalHeader className="modalHeader">
+                                <img width={48} height={48} src={button.url} alt="Technology Icon"/>
+                                <text>{button.technology}</text>
+                            </ModalHeader>
+                            <ModalCloseButton/>
+                            <ModalBody className="modalBody">
+                                <text>{button.description}</text>
+                            </ModalBody>
+                        </ModalContent>
+                    </Modal>
+                </div>
             );
         });
         return b;
